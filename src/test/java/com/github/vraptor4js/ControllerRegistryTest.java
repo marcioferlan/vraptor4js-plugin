5095fb1417f82f34241c83a6f1bc2cbf3f65bce0
Denilson Telaroli <ddts80@gmail.com>
2014-11-07 20:00:54 -0200

    Tests for ControllerRegistry

---
+++ src/test/java/com/github/vraptor4js/ControllerRegistryTest.java
@@ -0,0 +1,63 @@
+package com.github.vraptor4js;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.inject.spi.AnnotatedConstructor;
+import javax.enterprise.inject.spi.AnnotatedField;
+import javax.enterprise.inject.spi.AnnotatedMethod;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.vraptor.Controller;
+
+public class ControllerRegistryTest {
+
+	private ControllerRegistry controllerRegistry;
+
+	@Controller
+	static class MyController {
+		@V4js
+		public void myMethod() {}
+		public void myMethodWithoutAnnotation() {}
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		controllerRegistry = new ControllerRegistry();
+	}
+
+	@Test
+	public void shouldRegistryAction() {
+		ProcessAnnotatedType<MyController> type = new ProcessAnnotatedType<MyController>() {
+			public AnnotatedType<MyController> getAnnotatedType() {
+				return new AnnotatedType<ControllerRegistryTest.MyController>() {
+					@Override public Type getBaseType() {return null;}
+					@Override public Set<Type> getTypeClosure() {return null;}
+					@Override public <T extends Annotation> T getAnnotation(Class<T> annotationType) {return null;}
+					@Override public Set<Annotation> getAnnotations() {return null;}
+					@Override public Set<AnnotatedField<? super MyController>> getFields() {return null;}
+					@Override public Set<AnnotatedConstructor<MyController>> getConstructors() {return null;}
+					@Override public Class<MyController> getJavaClass() {return MyController.class;}
+					@Override public Set<AnnotatedMethod<? super MyController>> getMethods() {return new HashSet<>();}
+					@Override public boolean isAnnotationPresent(Class<? extends Annotation> annotationType) {return true;}
+				};
+			}
+			public void veto() {}
+			@Override public void setAnnotatedType(AnnotatedType<MyController> type) {}
+		};
+		
+		controllerRegistry.scanControllers(type);
+		
+		assertThat(controllerRegistry.getActions("MyController").get(0).getName(), equalTo("myMethod"));
+		assertThat(controllerRegistry.getActions("MyController").size(), equalTo(1));
+	}
+}
