332f5e60e8798a685860639a5b9e87ab62f90b36
Marcio Lima <marcio@gtisolucoes.com>
2015-04-28 23:57:32 -0300

    Removing X-Fowarded-Proto support (should be set in the app server)

--- src/main/java/com/gtisolucoes/vraptor4js/ControllerLinker.java
+++ src/main/java/com/gtisolucoes/vraptor4js/ControllerLinker.java
@@ -4,9 +4,6 @@ import java.lang.reflect.Method;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang.StringUtils;
 
 import br.com.caelum.vraptor.config.Configuration;
 import br.com.caelum.vraptor.http.route.Router;
@@ -22,20 +19,18 @@ public class ControllerLinker {
 
 	private final Router router;
 	private final Configuration cfg;
-	private final HttpServletRequest request;
 
 	/**
 	 * @deprecated CDI eyes-only
 	 */
 	protected ControllerLinker() {
-		this(null, null, null);
+		this(null, null);
 	}
 	
 	@Inject
-	public ControllerLinker(Router router, Configuration cfg, HttpServletRequest request) {
+	public ControllerLinker(Router router, Configuration cfg) {
 		this.router = router;
 		this.cfg = cfg;
-		this.request = request;
 	}
 
 	/**
@@ -47,17 +42,7 @@ public class ControllerLinker {
 	public String linkTo(final Class<?> controller, final Method method) {
 		String uri = router.urlFor(controller, method, new Object[method.getParameterTypes().length]);
 		if (uri.startsWith("/")) {
-			// full url path
-			final String url = cfg.getApplicationPath() + uri;
-			// checking for X-Forwarded-Proto header
-			final String X_Forwarded_Proto = request.getHeader("X-Forwarded-Proto");
-			final String protocol = request.getScheme();
-			// X-Forwarded-Proto header is different from actual request scheme (protocol)
-			if (StringUtils.isNotEmpty(X_Forwarded_Proto) && !X_Forwarded_Proto.equals(protocol)) {
-				// replace request scheme by t
-				return url.replace(protocol, X_Forwarded_Proto);
-			}
-			return url;
+			return cfg.getApplicationPath() + uri;
 		}
 		return uri;
 	}
--- src/test/java/com/gtisolucoes/vraptor4js/ControllerLinkerTest.java
+++ src/test/java/com/gtisolucoes/vraptor4js/ControllerLinkerTest.java
@@ -6,14 +6,10 @@ import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Method;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import com.gtisolucoes.vraptor4js.ControllerLinker;
 
 import br.com.caelum.vraptor.config.Configuration;
 import br.com.caelum.vraptor.http.route.Router;
@@ -23,7 +19,6 @@ public class ControllerLinkerTest {
 	private ControllerLinker controllerLinker;
 	@Mock private Router router;
 	@Mock private Configuration cfg;
-	@Mock private HttpServletRequest request;
 	private Method method;
 	private Class<MyController> controller;
 	
@@ -35,7 +30,7 @@ public class ControllerLinkerTest {
 		method = controller.getMethod("myMethod");
 		when(cfg.getApplicationPath()).thenReturn("/path");
 		
-		controllerLinker = new ControllerLinker(router, cfg, request);
+		controllerLinker = new ControllerLinker(router, cfg);
 	}
 
 	@Test
@@ -51,16 +46,5 @@ public class ControllerLinkerTest {
 		
 		assertThat(controllerLinker.linkTo(controller, method), equalTo("my/myMethod"));
 	}
-	
-	@Test
-	public void shouldReplaceProtocol() {
-		when(cfg.getApplicationPath()).thenReturn("http://path");
-		when(router.urlFor(controller, method)).thenReturn("/my/myMethod");
-		when(request.getHeader("X-Forwarded-Proto")).thenReturn("https");
-		when(request.getScheme()).thenReturn("http");
-
-		assertThat(controllerLinker.linkTo(controller, method), equalTo("https://path/my/myMethod"));
-	}
-
 	
 }
